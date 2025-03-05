01a8ea6efcf9c24465cd563f82995e85c6964e04
megan6Degree <megan@6degrees.team>
2025-03-05 16:12:50 -0600

    server w/ heroku

--- server.js
+++ server.js
@@ -138,4 +138,5 @@ app.post('/upload', upload.single('file'), async (req, res) => {
 // Mount user routes
 app.use('/api/users', userRoutes)
 
-app.listen(3001, () => console.log('Server running on port 3001'))
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
